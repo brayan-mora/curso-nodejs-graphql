9cff5c8ca20efe316fe58825ae5a79fc53bfba1c
Brayan Mora <brayan.mora@infracommerce.lat>
2024-06-24 11:21:01 -0500

    Implement dynamic nesting for categories to fetch related products on demand

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,7 +1,8 @@
-const {getProduct, getProducts, addProduct, updateProduct, deleteProduct} = require('./product.resolvers')
+const {getProduct, getProducts, addProduct, updateProduct, deleteProduct, getProductsByCategory} = require('./product.resolvers')
 const { login } = require('./auth.resolvers') 
-const { addCategory } = require('./category.resolvers') 
+const { addCategory, getCategory } = require('./category.resolvers') 
 const { RegularExpression } = require('graphql-scalars');
+// const { Category } = require('../db/models/category.model');
 
 const CategoryNameType = new RegularExpression('CategoryNameType', /^[a-zA-Z0-9]{3,8}$/)
 
@@ -17,7 +18,8 @@ const resolvers = {
         getNumbers: (_, args) => args.numbers,
         // Products
         product: getProduct,
-        products: getProducts
+        products: getProducts,
+        category: getCategory
     },
     Mutation: {
         login,
@@ -26,7 +28,10 @@ const resolvers = {
         deleteProduct,
         addCategory
     },
-    CategoryNameType
+    CategoryNameType,
+    Category: {
+        products: getProductsByCategory
+    }
 };
 
 module.exports = resolvers;
